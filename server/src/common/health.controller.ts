7cfb9de864c77ab729078760a2ed9e32f0cffe2b
Howlil <2211521003_ulil@student.unand.ac.id>
2026-06-10 05:14:22 +0700

    update

--- server/src/common/health.controller.ts
+++ server/src/common/health.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, VERSION_NEUTRAL, Version } from '@nestjs/common';
 
 @Controller('health')
 export class HealthController {
   @Get()
+  @Version(VERSION_NEUTRAL)
   check() {
     return {
       status: 'ok',
